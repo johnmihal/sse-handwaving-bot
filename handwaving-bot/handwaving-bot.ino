f341a97bec27dbff6252cd09c6fc72452365e6c9
John Mihal <johnmihal34@gmail.com>
2023-12-18 00:31:53 -0500

    final code

--- handwaving-bot/handwaving-bot.ino
+++ handwaving-bot/handwaving-bot.ino
@@ -2,19 +2,40 @@
 Servo myServo;
 
 int const butPin = 7;
+int const sensPin = 8;
 int angle1 = 45;
 int angle2 = 135;
 
 void setup() {
   pinMode(butPin, INPUT);
+  pinMode(sensPin, INPUT);
+
   myServo.attach(9);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  if (digitalRead(butPin) == HIGH){
+  if (digitalRead(butPin) == HIGH) {
     wave();
+  }
+
+  if (digitalRead(sensPin) == HIGH) {
+    wave();
+  }
+
+  // if (digitalRead(butPin) == HIGH){
+  //   Serial.println("1");
+  // } else {
+  //   Serial.println("0");
+  // }
+
+
+
+  if (digitalRead(sensPin) == HIGH){
+    Serial.println("MOTION DETECTED");
+  } else {
+    Serial.println("NO MOTION");
   }
 }
 
@@ -28,14 +49,14 @@ void wave(){
     myServo.write(angle1);
     // Serial.print("angle: ");
     // Serial.println(angle1);
-    delay(30);
+    delay(1000);
     
     myServo.write(angle2);
     // Serial.print("angle: ");
     // Serial.println(angle2);
-    delay(30);
+    delay(1000);
 
-    time+=60;
+    time+=2000;
   }
 
 }
